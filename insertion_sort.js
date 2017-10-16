5c4bcd3a21667846e5e97a4eda676f2672ea8c1c
Pedro Henrique Guimarães <pedroguimaraes.contato@gmail.com>
2017-10-15 22:48:26 -0200

    Update insertion_sort.js

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,10 +1,10 @@
 /**
 * Insertion sort works like a card game, where you have to
-* sort the cards on crescent order. When you take a new card
-* you probably will looks on your hand to see the order. 
-* If the left card is bigger than card on your hand, so you 
-* have to put you card on the same position that left card is
-* and the left card will increment (+1) your position. 
+* sort the cards in ascending order. When you pick up a new card
+* you'll probably looks at your hand to see the current order. 
+* If the left card is bigger than the card in your hand, so you 
+* have to put you card at the same position that left card is.
+* And the left card will increase (+1) your position. 
 */
 
 function crescent_insertion_sort(vector) {
@@ -13,10 +13,10 @@ function crescent_insertion_sort(vector) {
 	var j;
 	var aux; 
 
-	/* We start on index 1, 
-	   because for induction our first index (0) 
+	/* We start at index 1, 
+	   by induction our first index (0) 
 	   is already sorted... because we don't have
-	   nothing on left side. 
+	   anything on the left side. 
 	*/ 
 
 	for (i = 1; i < vector.length; i++) {
@@ -38,13 +38,7 @@ function decrescent_insertion_sort(vector) {
 	var i; 
 	var j;
 	var aux; 
-
-	/* We start on index 1, 
-	   because for induction our first index (0) 
-	   is already sorted... because we don't have
-	   nothing on left side. 
-	*/ 
-
+	
 	for (i = 1; i < vector.length; i++) {
 		aux = vector[i];
 		j = i - 1; 
